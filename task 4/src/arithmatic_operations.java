d3ebb041627b01ed84a36a99a5e0a9dda2013548
Ayesha Tariq <ayeshatariq1538@gmail.com>
2025-10-15 06:44:27 -0700

    Update arithmatic_operations.java

--- task 4/src/arithmatic_operations.java
+++ task 4/src/arithmatic_operations.java
@@ -3,7 +3,7 @@ class arithmatic_operations {
 
 	 public static void main(String[] args) {
 	        int a = 10, b = 5;
-	        System.out.println("Add: " + (a + b));
+	        System.out.println("Addition: " + (a + b));
 	        System.out.println("Sub: " + (a - b));
 	        System.out.println("Mul: " + (a * b));
 	        System.out.println("Div: " + (a / b));
