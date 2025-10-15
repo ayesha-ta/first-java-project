1f938ab3fc094eb58160c0e0647f694ebef46b35
Ayesha Tariq <ayeshatariq1538@gmail.com>
2025-10-15 06:24:30 -0700

    Update arithmatic_operations.java

--- task 4/src/arithmatic_operations.java
+++ task 4/src/arithmatic_operations.java
@@ -5,8 +5,8 @@ class arithmatic_operations {
 	        int a = 10, b = 5;
 	        System.out.println("Add: " + (a + b));
 	        System.out.println("Sub: " + (a - b));
-	        System.out.println("Multiplication: " + (a * b));
-	        System.out.println("Division: " + (a / b));
+	        System.out.println("Mul: " + (a * b));
+	        System.out.println("Div: " + (a / b));
 	        System.out.println("Remainder: " + (a % b));
 
 	}
