72ffb20dd7bfc83dc58dacd8a9f05403e68162af
Ayesha Tariq <ayeshatariq1538@gmail.com>
2025-10-23 08:24:57 -0700

    Update world.java

--- hello/src/hello/world.java
+++ hello/src/hello/world.java
@@ -4,7 +4,7 @@ class world {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to Real world !!! ");
+		System.out.println("Welcome to Real world  problems !!! ");
 
 	}
 
