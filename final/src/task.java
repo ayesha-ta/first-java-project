e6cb979807639f179f87958e834328f1b61a56cc
ayesha-ta <ayeshatariq1538@gmail.com>
2025-10-23 13:36:51 -0700

    committed task

---
+++ final/src/task.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ayesha
+ *
+ */
+class task {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+			System.out.println("ayesha tariq");
+		
+			
+			 for (int i = 1; i <= 20; i++) {
+		            System.out.println(i);
+		        }
+		    }
+
+	}
+
+
--- practisetask/src/practisetask/practise.java
+++ practisetask/src/practisetask/practise.java
@@ -2,13 +2,4 @@
 package practisetask;
 
 class practise {
-	public static void main(String[] args) {
-		System.out.println("ayesha tariq");
 	
-		
-		 for (int i = 1; i <= 20; i++) {
-	            System.out.println(i);
-	        }
-	    }
-
-}
